0cf1906722aa5153a6085ad8b309971e49a0e79a
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-10-18 15:09:16 -0400

    Brute-forcing D

--- main.c
+++ main.c
@@ -6,16 +6,6 @@
 #include <stdio.h>
 #include <math.h>
 
-
- // RSA parameters
-    /* P and Q must be prime numbers and must be very large in real production use */
-    #define P (3)
-    #define Q (11)
-    #define N (P * Q)
-    #define Totient ((P - 1) * (Q - 1)) /* Euler's totient function */
-    #define E (7)                       /* E must have only one common divisor with Totient */
-    #define D (3)
-
 // Modular exponentiation function to avoid integer overflow
 long long modExp(long long base, long long exp, long long mod) {
     long long result = 1;
@@ -32,11 +22,42 @@ long long modExp(long long base, long long exp, long long mod) {
     return result;
 }
 
+// Calculate modular multiplicative inverse
+long long modInverse(long long a, long long m) {
+    long long m0 = m, t, q;
+    long long x0 = 0;
+    long long x1 = 1;
+
+    while (a > 1) {
+         q = a / m;
+        t = m;
+        m = a % m, a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+     if (x1 < 0)
+        x1 += m0;
+
+    return x1;
+}
+
 int main(void) {
     int i;
     int msg[4] = {12, 15, 22, 5};
     long en[4], de[4];
     int SecCode[4], DeMsg[4];
+
+    // RSA parameters
+    /* P and Q must be prime numbers and must be very large in real production use */
+    #define P (3)
+    #define Q (11)
+    #define N (P * Q)
+    #define Totient ((P - 1) * (Q - 1)) /* Euler's totient function */
+    #define E (7)                       /* E must have only one common divisor with Totient */
+    long long D = modInverse(E, Totient);
+
 
     printf("The following is a simple demonstration of the RSA encryption and decryption algorithm:\n");
     printf("\t Copyright(C) Asadbek Karimov.\n\n");
